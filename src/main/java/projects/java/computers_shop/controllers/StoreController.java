4272d17fb19e271bb916a25d614c76076f918f95
tizzifona <tizzifona@gmail.com>
2024-12-17 15:38:10 +0100

    Added code for StoreController.java

--- src/main/java/projects/java/computers_shop/controllers/StoreController.java
+++ src/main/java/projects/java/computers_shop/controllers/StoreController.java
@@ -1,5 +1,34 @@
 package projects.java.computers_shop.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import projects.java.computers_shop.dtos.StoreDTO;
+import projects.java.computers_shop.models.Store;
+import projects.java.computers_shop.services.StoreService;
+
+@RestController
+@RequestMapping("/stores")
 public class StoreController {
 
+    @Autowired
+    private StoreService storeService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Store> addStore(@RequestBody StoreDTO storeDTO) {
+        Store store = storeService.addStore(storeDTO);
+        return ResponseEntity.ok(store);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StoreDTO> getStoreById(@PathVariable Long id) {
+        StoreDTO storeDTO = storeService.getStoreById(id);
+        return ResponseEntity.ok(storeDTO);
+    }
 }
