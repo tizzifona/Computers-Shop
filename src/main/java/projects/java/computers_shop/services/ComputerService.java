06663b1f821f14c6cbea3a3a7e97240de8ab2932
tizzifona <tizzifona@gmail.com>
2024-12-17 15:04:01 +0100

    Added code for ComputerController and ComputerService, using ComputerDTO

--- src/main/java/projects/java/computers_shop/services/ComputerService.java
+++ src/main/java/projects/java/computers_shop/services/ComputerService.java
@@ -1,11 +1,13 @@
 package projects.java.computers_shop.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import projects.java.computers_shop.models.Computer;
+import projects.java.computers_shop.dtos.ComputerDTO;
 import projects.java.computers_shop.repositories.ComputerRepository;
 
 @Service
@@ -14,19 +16,59 @@ public class ComputerService {
     @Autowired
     private ComputerRepository computerRepository;
 
-    public List<Computer> getAllComputers() {
-        return computerRepository.findAll();
+    public List<ComputerDTO> getAllComputers() {
+        return computerRepository.findAll()
+                .stream()
+                .map(computer -> new ComputerDTO(
+                        computer.getBrand(),
+                        computer.getMemory(),
+                        computer.getProcessor(),
+                        computer.getOperatingSystem(),
+                        computer.getPrice()))
+                .collect(Collectors.toList());
     }
 
-    public Computer addComputer(Computer computer) {
-        return computerRepository.save(computer);
+    public ComputerDTO addComputer(ComputerDTO computerDTO) {
+        Computer computer = new Computer(
+                null,
+                computerDTO.brand(),
+                computerDTO.memory(),
+                computerDTO.processor(),
+                computerDTO.operatingSystem(),
+                computerDTO.price());
+
+        Computer savedComputer = computerRepository.save(computer);
+        return new ComputerDTO(
+                savedComputer.getBrand(),
+                savedComputer.getMemory(),
+                savedComputer.getProcessor(),
+                savedComputer.getOperatingSystem(),
+                savedComputer.getPrice());
     }
 
-    public Computer deleteComputerByBrand(String brand) {
-        return computerRepository.deleteByBrand(brand);
+    public ComputerDTO deleteComputerByBrand(String brand) {
+        Computer computer = computerRepository.deleteByBrand(brand);
+        if (computer != null) {
+            return new ComputerDTO(
+                    computer.getBrand(),
+                    computer.getMemory(),
+                    computer.getProcessor(),
+                    computer.getOperatingSystem(),
+                    computer.getPrice());
+        }
+        return null;
     }
 
-    public Computer findComputerByBrand(String brand) {
-        return computerRepository.findByBrand(brand);
+    public ComputerDTO findComputerByBrand(String brand) {
+        Computer computer = computerRepository.findByBrand(brand);
+        if (computer != null) {
+            return new ComputerDTO(
+                    computer.getBrand(),
+                    computer.getMemory(),
+                    computer.getProcessor(),
+                    computer.getOperatingSystem(),
+                    computer.getPrice());
+        }
+        return null;
     }
 }
