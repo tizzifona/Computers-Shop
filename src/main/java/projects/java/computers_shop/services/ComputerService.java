74fb3b1c7081b44b2d7f53df707ab54899972f8a
tizzifona <tizzifona@gmail.com>
2024-12-17 09:58:24 +0100

    Added code for ComputerService.java

--- src/main/java/projects/java/computers_shop/services/ComputerService.java
+++ src/main/java/projects/java/computers_shop/services/ComputerService.java
@@ -1,5 +1,33 @@
 package projects.java.computers_shop.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import projects.java.computers_shop.models.Computer;
+import projects.java.computers_shop.repositories.ComputerRepository;
+
+@Service
 public class ComputerService {
 
+    private ComputerRepository computerRepository;
+
+    @Autowired
+
+    public List<Computer> getAllComputers() {
+        return computerRepository.findAll();
+    }
+
+    public Computer addComputer(Computer computer) {
+        return computerRepository.save(computer);
+    }
+
+    public Computer deleteComputerByBrand(String brand) {
+        return computerRepository.deleteByBrand(brand);
+    }
+
+    public Computer findComputerByBrand(String brand) {
+        return computerRepository.findByBrand(brand);
+    }
 }
